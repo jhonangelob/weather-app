231c0f7e222e58ef00e308c3f63f0a01bba0f087
jhonangeloB <bustarde.aj@gmail.com>
2022-12-14 01:43:13 +0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { BsCloudDrizzle } from 'react-icons/bs';
 import { GrSearch } from 'react-icons/gr';
 import { WiHumidity, WiRain, WiRainWind, WiThermometer } from 'react-icons/wi';
@@ -7,61 +7,52 @@ import './App.css';
 function App() {
   const [loading, setLoading] = useState(false);
   const inputRef = useRef(null);
-  const [weather, setWeather] = useState();
+  const [weather, setWeather] = useState(() => {
+    setLoading(true);
+    navigator.geolocation.getCurrentPosition((position) => {
+      getLocation(position.coords.latitude, position.coords.longitude).then(
+        (result) => {
+          setLoading(false);
+          setWeather(result);
+        }
+      );
+    });
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+
     const searchLocation = inputRef.current.value.trim();
-    getWeather(searchLocation).then((result) => {
-      setWeather(result);
-      setLoading(false);
-      console.log(weather);
-    });
+    getWeather(searchLocation)
+      .then((result) => getLocation(result.latitude, result.longitude))
+      .then((result) => {
+        setWeather(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+
     e.target.reset();
   };
 
-  const getCurrentLocation = async () => {
-    setLoading(true);
-    await navigator.geolocation.getCurrentPosition(
-      (position) => {
-        getLocation(position.coords.latitude, position.coords.longitude).then(
-          (result) => {
-            setWeather(result);
-            setLoading(false);
-          }
-        );
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  };
-
   const getWeather = async (searchLocation) => {
-    try {
-      const data = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${searchLocation}&limit=1&appid=${process.env.REACT_APP_API_KEY}`
-      );
-      setLoading(true);
-      const coordinates = await data.json();
-
-      return getLocation(coordinates[0].lat, coordinates[0].lon);
-    } catch (error) {
-      console.error(`Error fetching data from API`);
-    }
+    const data = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${searchLocation}&limit=1&appid=${process.env.REACT_APP_API_KEY}`
+    );
+    const coordinates = await data.json();
+    return { latitude: coordinates[0].lat, longitude: coordinates[0].lon };
   };
 
   const getLocation = async (latitude, longitude) => {
     const location = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`
     );
-    const result = location.json();
+    const result = await location.json();
     return result;
   };
-
-  useEffect(() => {
-    getCurrentLocation();
-  }, []);
 
   return (
     <div className='App flex flex-col bg-bg-color min-h-screen justify-center items-center gap-6 md:w-3/4 md:mx-auto'>
